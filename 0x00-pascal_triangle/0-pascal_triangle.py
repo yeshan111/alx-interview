2060c07076ee34cca52e4aea7c423dba4368cfa2
yeshan111 <yeshan111@gmail.com>
2023-03-21 05:24:30 -0700

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python3
+'''
+Returns list representing the Pascal's triangle of n
+'''
+
+
+
 def pascal_triangle(n):
+    '''returns empty list if n <= 0'''
     if n <= 0:
         return []
+
+
     triangle = [[1]]
     for i in range(1, n):
         prev_row = triangle[-1]
